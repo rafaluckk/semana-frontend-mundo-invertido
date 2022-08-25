43fd3ac7a57d94b54edf937ceb4d537d6b61049d
rafaluckk <rafael2021516@gmail.com>
2022-08-25 15:02:52 -0300

    git

---
+++ assets/js/data/firebase.js
@@ -0,0 +1,20 @@
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyBmmcSkT5SbxYDLRLGmtEqcyhy83q3lmYc",
+    authDomain: "projeto-mundo-invertido-4b472.firebaseapp.com",
+    projectId: "projeto-mundo-invertido-4b472",
+    storageBucket: "projeto-mundo-invertido-4b472.appspot.com",
+    messagingSenderId: "736634547667",
+    appId: "1:736634547667:web:1845d964eb029a738dd326",
+    measurementId: "G-VEKR10BM2R"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  export default app

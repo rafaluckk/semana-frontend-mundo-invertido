522e5daeee3408b084a977ae8e7096a9af0438c7
rafaluckk <rafael2021516@gmail.com>
2022-08-26 17:42:41 -0300

    git

--- assets/js/data/config.js
+++ assets/js/data/config.js
@@ -1,3 +1,9 @@
-const firebaseConfig = {};
+const firebaseConfig = {apiKey: "AIzaSyBmmcSkT5SbxYDLRLGmtEqcyhy83q3lmYc",
+authDomain: "projeto-mundo-invertido-4b472.firebaseapp.com",
+projectId: "projeto-mundo-invertido-4b472",
+storageBucket: "projeto-mundo-invertido-4b472.appspot.com",
+messagingSenderId: "736634547667",
+appId: "1:736634547667:web:1845d964eb029a738dd326",
+measurementId: "G-VEKR10BM2R"};
 
 export default firebaseConfig;
